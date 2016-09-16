40b27777bf3bc33898202fc4c8d0e7d9442b3dad
Neha Samant <nsamant@nfml-nsamantsqg.corp.netflix.com>
2016-09-16 15:25:55 -0700

    added options to settings screen

--- TipCalculator/SettingsViewController.swift
+++ TipCalculator/SettingsViewController.swift
@@ -9,18 +9,35 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-
+    
+    @IBOutlet weak var tipSlider: UISlider!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var tipDefaultLabel: UILabel!
+    @IBOutlet weak var spiltCountLabel: UILabel!
+    @IBOutlet weak var spiltControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let defaults = NSUserDefaults.standardUserDefaults()
-        defaults.setInteger(10, forKey: "tip")
-        defaults.synchronize()
+        setDefaultTip(15)
+        setDefaultSplitBy(0)
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func setDefaultTip(tip : Int){
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setInteger(tip, forKey: "tip")
+        defaults.synchronize()
+    }
+    
+    func setDefaultSplitBy(split : Int){
+        let defaults = NSUserDefaults.standardUserDefaults()
+         defaults.setInteger(split, forKey: "splitBy")
+        defaults.synchronize()
     }
     
     /*
@@ -33,4 +50,15 @@ class SettingsViewController: UIViewController {
     }
     */
 
+    @IBAction func sliderValueChanged(sender: UISlider) {
+        var currentValue = Int(sender.value)
+        tipLabel.text = "\(currentValue)%"
+        setDefaultTip(currentValue)
+    }
+    
+    @IBAction func spiltValueChanged(sender: AnyObject) {
+        spiltCountLabel.text =  String(spiltControl.selectedSegmentIndex)
+        print("what is the value" ,spiltControl.selectedSegmentIndex)
+        setDefaultSplitBy(spiltControl.selectedSegmentIndex)
+    }
 }
--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -13,13 +13,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var billField: UITextField!
-    @IBOutlet weak var tipControl: UISegmentedControl!
-    
-    let tipValue: Int
+    @IBOutlet weak var spiltTotal: UILabel!
+    @IBOutlet weak var spiltLabel: UILabel!
+    @IBOutlet weak var tipValueLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("view did load")
+        getDefaultValue()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -31,15 +32,9 @@ class ViewController: UIViewController {
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         print("view will appear")
-        getDefaultValues()
+        getDefaultValue()
     }
-    
-    func getDefaultValues(){
-        let defaults = NSUserDefaults.standardUserDefaults()
-        let tipValue = defaults.integerForKey("tip")
-        print(tipValue)
-    }
-    
+   
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         print("view did appear")
@@ -59,16 +54,38 @@ class ViewController: UIViewController {
         
         view.endEditing(true)
     }
+    
+    func getDefaultValue(){
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let tipPercent = defaults.integerForKey("tip")
+        let splitValue = defaults.integerForKey("splitBy")
+        tipLabel.text = "Tip(\(tipPercent)%)"
+        spiltLabel.text = String(format: "Split (\(Int(splitValue)))")
+    }
 
     @IBAction func calculateTip(sender: AnyObject) {
-        getDefaultValues()
-        let tipPercentages = [0.18, 0.2, 0.22]
+        calculateTip()
+    }
+    
+    func calculateTip(){
         
         let bill = Double(billField.text!) ?? 0
-        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let tipPercent = NSUserDefaults.standardUserDefaults().integerForKey("tip")
+        let tip = (bill * Double(tipPercent))/100
         let total = bill + tip
-        tipLabel.text = String(format: "$%0.2f", tip)
+        
+        tipLabel.text = "Tip(\(tipPercent)%)"
+        tipValueLabel.text = String(format: "$%0.2f", tip)
         totalLabel.text = String(format: "$%0.2f", total)
+        
+        let splitBy = Double(NSUserDefaults.standardUserDefaults().integerForKey("splitBy"))
+        if(splitBy != 0){
+            let splitAmount = total/splitBy
+            spiltLabel.text = String(format: "Split (\(Int(splitBy)))")
+            spiltTotal.text = String(format: "$%0.2f", splitAmount)
+        }else {
+            spiltTotal.text = String(format: "$%0.2f", total)
+        }
     }
 }
 
